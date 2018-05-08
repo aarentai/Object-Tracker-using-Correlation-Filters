fd59e1f2adf068b00ca07dd65b3893e352794284
aarentai <aarentai@tongji.edu.cn>
2018-05-08 16:37:07 +0800

    Update mosse.m

--- mosse.m
+++ mosse.m
@@ -1,81 +1,76 @@
 %data_path should include 'img'(including all the frames in a certain sequence) and roundtruth_rect.txt
-data_path = './Basketball';
+data_path = '.\Tiger2';
 
 %sequence_path, namely 'img', should include all the frames of a certain sequence
-sequence_path = [data_path '/img/'];
-sequence_path_dir = dir(sequence_path);
-
-%read the exact position and size [x y w h] of the target in the first frame from groundtruth_rect.txt
-%file = fopen([data_path '/groundtruth_rect.txt']);
-%ground_truth = textscan(file, '%f,%f,%f,%f');
-%fclose(file);
-ground_truth = [198 214 34 81];
-
-%save the names of frames in 'img' on frame_name_content
-frame_name_content = cell(length(sequence_path_dir) - 2,1);
-for i = 1 : (length(sequence_path_dir) - 2)
-	frame_name_content{i} = sequence_path_dir(i + 2).name;
-end
+[gt,sequence_path,img_files]=Load_image(data_path);
 
 %read the first frame
-first_frame = imread([sequence_path frame_name_content{1}]);
-%first_frame = imread('./Basketball/img/0001.jpg');
+img = imread([sequence_path img_files{1}]);
+
+%assign the first frame's ground truth
+ground_truth = [32 60 68 78];
+endFrame=length(img_files);
+
 %turn rgb to gray
-if size(first_frame,3) > 1
-		first_frame = rgb2gray(first_frame);
+if (size(img,3) == 1)
+	frame = img;
+else
+	frame = rgb2gray(img);
 end
+
 %target_sz = [w h], which denote the width and height, respectively.
-target_sz = [ground_truth(4), ground_truth(3)];
+target_sz = [ground_truth(1,4), ground_truth(1,3)];
+
 %target_pos = [y x], which denote the position of target's central point, respectively.
-target_pos = [ground_truth(2), ground_truth(1)] + floor(target_sz/2);
+target_pos = [ground_truth(1,2), ground_truth(1,1)] + floor(target_sz/2);
 
 %ideal gaussian template
-gauss_response = gaussian_template(target_sz, first_frame);
+gauss_response = gaussian_template(target_sz, frame);
 
-update_visualization = show_video(frame_name_content, sequence_path);%%%%%%%%%%%%%%%%%%%
-
-for n = 1 : 725%length(sequence_path)
+for n = 1 : endFrame%length(sequence_path)
 	%read the n th frame into 'frame'
-	frame = imread([sequence_path frame_name_content{n}]);
+	img = imread([sequence_path img_files{n}]);
     %turn rgb to gray
-    if size(frame,3) > 1
-		frame = rgb2gray(frame);
+    if (size(img,3) == 1)
+		frame = img;
+	else
+		frame = rgb2gray(img);
     end
+    
 	%get the subwindow at the target position of last frame
 	target_last_frame = get_subwindow(target_pos, target_sz, frame);
-	
+    
 	if n > 1
 		%calculate response of the classifier at all locations
 		response = real(ifft2(H .* fft2(target_last_frame)));   
 		
 		%target location is at the maximum response
 		[row, col] = find(response == max(response(:)), 1);
-		target_pos = target_pos - floor(target_sz/2) + [row, col];
+		target_pos = target_pos - target_sz/2 + [row, col];
 	end
 	
 	F = fft2(get_subwindow(target_pos, target_sz, frame));
-	H = conj(F.*conj(gauss_response)./F.*conj(F));
+	H = conj(F.*conj(gauss_response)./(F.*conj(F)+eps));
 	
-	%visualization
-% 	rect_position = [target_pos([2,1]) - target_sz([2,1])/2, target_sz([2,1])];
-% 	if n == 1  %first frame, create GUI
-% 		%figure('Number','off', 'Name','tracker');
-%         figure('Name','tracker');
-% 		im_handle = imshow(frame, 'Border','tight', 'InitialMag',200);
-% 		rect_handle = rectangle('Position',rect_position, 'EdgeColor','g');
-% 	else
-% 		try  %subsequent frames, update GUI
-% 			set(im_handle, 'CData', frame)
-% 			set(rect_handle, 'Position', rect_position)
-% 		catch  %#ok, user has closed the window
-% 			return
-% 		end
-%     end
-    
     %visualization
-	box = [target_sz([2,1]) - target_sz([2,1])/2, target_sz([2,1])];
-	stop = update_visualization(n, box);
-	if stop, break, end  %user pressed Esc, stop early	
-	drawnow
-% 	pause(0.05)  %uncomment to run slower
+	box = [target_pos([2,1]) - target_sz([2,1])/2, target_sz([2,1])];
+
+	% visiualization   
+    if n == 1  %first frame, create GUI
+            figure
+            im_handle = imagesc(uint8(img));%img
+            rect_handle = rectangle('Position',box,'LineWidth',2,'EdgeColor','r');
+            tex_handle = text(5, 18, strcat('#',num2str(n)), 'Color','y', 'FontWeight','bold', 'FontSize',20);
+            drawnow;
+    else
+        try  %subsequent frames, update GUI
+			set(im_handle, 'CData', img)%img
+			set(rect_handle, 'Position', box)
+            set(tex_handle, 'string', strcat('#',num2str(n)))
+            pause(0.04);
+            drawnow;
+		catch  % #ok, user has closed the window
+			return
+        end
+    end
 end
